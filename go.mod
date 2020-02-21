352f45308ba10d29cfcac2e8e6621c1f5ebf4bec
Atsu-Imo <runo1go@gmail.com>
2020-02-22 01:36:42 +0900

    とりあえずコミット

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Atsu-Imo/line-golang
+
+go 1.13
+
+require (
+	cloud.google.com/go v0.53.0
+	github.com/line/line-bot-sdk-go v7.1.0+incompatible
+	google.golang.org/genproto v0.0.0-20200218151345-dad8c97a84f5
+)
---
+++ webhook.go
@@ -0,0 +1,87 @@
+package linebot
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	cloudkms "cloud.google.com/go/kms/apiv1"
+	"github.com/line/line-bot-sdk-go/linebot"
+	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
+)
+
+var (
+	lineInfo LineInfo
+)
+
+// LineInfo LineのMessagingAPIを使用するためのあれこれ
+type LineInfo struct {
+	LineSecret string `json: "line_secret`
+	LineToken  string `json: "line_token"`
+}
+
+func init() {
+	encode, err := ioutil.ReadFile("secrets.json.enc")
+	if err != nil {
+		log.Fatal("failed loading lineInfo", err)
+		return
+	}
+	ctx := context.Background()
+	client, err := cloudkms.NewKeyManagementClient(ctx)
+	if err != nil {
+		log.Fatal("failed loading lineInfo", err)
+		return
+	}
+	req := &kmspb.DecryptRequest{
+		Name:       lineSecretsKmsKeyName(),
+		Ciphertext: encode,
+	}
+	resp, err := client.Decrypt(ctx, req)
+	if err != nil {
+		log.Fatal("failed loading lineInfo", err)
+		return
+	}
+	secretJSON := resp.Plaintext
+	if err := json.Unmarshal(secretJSON, &lineInfo); err != nil {
+		log.Fatal("failed json unmarshal secrets", err)
+		return
+	}
+}
+func lineSecretsKmsKeyName() string {
+	prjID := os.Getenv("GCP_PROJECT_ID")
+	keyRingName := os.Getenv("KMS_KEY_RING_NAME")
+	keyName := os.Getenv("KMS_LINE_SECRETS_KEY_NAME")
+	return fmt.Sprintf("projects/%s/locations/global/keyRings/%s/cryptoKeys/%s", prjID, keyRingName, keyName)
+}
+
+// Webhook Lineから呼び出される
+func Webhook(w http.ResponseWriter, r *http.Request) {
+	client, err := linebot.New(lineInfo.LineSecret, lineInfo.LineToken)
+	if err != nil {
+		http.Error(w, "Error: init line client", http.StatusBadRequest)
+		log.Fatal(err)
+		return
+	}
+	events, err := client.ParseRequest(r)
+	if err != nil {
+		http.Error(w, "Error: parse Request", http.StatusBadRequest)
+		log.Fatal(err)
+		return
+	}
+	for _, e := range events {
+		switch e.Type {
+		case linebot.EventTypeMessage:
+			message := linebot.NewTextMessage("Test")
+			_, err := client.ReplyMessage(e.ReplyToken, message).Do()
+			if err != nil {
+				log.Fatal("Error: messaging", err)
+				continue
+			}
+		}
+	}
+	fmt.Println(w, "ok")
+}
